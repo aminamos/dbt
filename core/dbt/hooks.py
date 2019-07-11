1cbcfc8000bf66658b9ab24023002053809f7e3f
Jacob Beck <jake@fishtownanalytics.com>
2019-07-11 11:00:50 -0600

    remove unused function

--- core/dbt/hooks.py
+++ core/dbt/hooks.py
@@ -3,7 +3,6 @@ import json
 
 from dbt.contracts.graph.parsed import Hook
 
-import dbt.exceptions
 from typing import Union, Dict, Any
 
 
@@ -28,23 +27,3 @@ def get_hook(source, index):
     hook_dict = get_hook_dict(source)
     hook_dict.setdefault('index', index)
     return Hook.from_dict(hook_dict)
-
-
-def get_hooks(model, hook_key):
-    if hook_key == ModelHookType.PreHook:
-        hooks = model.config.pre_hook
-    elif hook_key == ModelHookType.PostHook:
-        hooks = model.config.post_hook
-    else:
-        raise dbt.exceptions.InternalException(
-            'Got invalid hook key {}'.format(hook_key)
-        )
-
-    # should this be:
-    # return [dataclasses.replace(Hook, index=i)
-    #         for i, hook in enumerate(hooks)]
-    # ???
-    # should this exist at all?
-
-    wrapped = [get_hook(hook, i) for i, hook in enumerate(hooks)]
-    return wrapped
